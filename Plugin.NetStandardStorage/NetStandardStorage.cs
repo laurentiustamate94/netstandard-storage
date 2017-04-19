588c06b440977c08a54b1f9c12f7bedc0a966ff2
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-20 00:32:39 +0300

    Add main entry point

---
+++ Plugin.NetStandardStorage/NetStandardStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Plugin.NetStandardStorage.Abstractions.Interfaces;
+using Plugin.NetStandardStorage.Implementations;
+
+namespace Plugin.NetStandardStorage
+{
+    public sealed class NetStandardStorage
+    {
+        private static Lazy<IFileSystem> _fileSystem
+            = new Lazy<IFileSystem>(() => new FileSystem(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        public static IFileSystem FileSystem
+        {
+            get
+            {
+                return _fileSystem.Value;
+            }
+        }
+
+        private NetStandardStorage()
+        {
+
+        }
+    }
+}
