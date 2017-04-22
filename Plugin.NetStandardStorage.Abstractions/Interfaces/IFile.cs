0369b63a06e2bd1a8dfc53e0a690c7be34550d90
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-22 18:20:53 +0300

    Add write methods on interface

--- Plugin.NetStandardStorage.Abstractions/Interfaces/IFile.cs
+++ Plugin.NetStandardStorage.Abstractions/Interfaces/IFile.cs
@@ -2,6 +2,7 @@
 /// Adapted from
 /// Windows.Foundation.UniversalApiContract, Version=4.0.0.0
 /// </summary>
+using System.Collections.Generic;
 using System.IO;
 using Plugin.NetStandardStorage.Abstractions.Types;
 
@@ -59,5 +60,26 @@ namespace Plugin.NetStandardStorage.Abstractions.Interfaces
         /// Deletes the current file.
         /// </summary>
         void Delete();
+
+        /// </summary>
+        /// <param name="path">The file to write to.</param>
+        /// <param name="bytes">The bytes to write to the file.</param>
+        void WriteAllBytes(byte[] bytes);
+
+        /// <summary>
+        /// Creates a new file, writes a collection of strings to the file, and then closes
+        /// the file.
+        /// </summary>
+        /// <param name="path">The file to write to.</param>
+        /// <param name="contents">The lines to write to the file.</param>
+        void WriteAllLines(IEnumerable<string> contents);
+
+        /// <summary>
+        /// Creates a new file, writes the specified string to the file, and then closes
+        /// the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <param name="path">The file to write to.</param>
+        /// <param name="contents">The string to write to the file.</param>
+        void WriteAllText(string contents);
     }
 }
