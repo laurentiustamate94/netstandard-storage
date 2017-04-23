26916b55722d27a17dd75c676274825d63f24f13
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-23 18:54:41 +0300

    Add FileSystem implementation for the Android project

---
+++ Plugin.NetStandardStorage.Android/Implementations/FileSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Plugin.NetStandardStorage.Abstractions.Interfaces;
+
+namespace Plugin.NetStandardStorage.Implementations
+{
+    public class FileSystem : IFileSystem
+    {
+        public IFolder LocalStorage
+        {
+            get
+            {
+                var localStorage = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+                return new Folder(localStorage, canDelete: false);
+            }
+        }
+
+        public IFolder RoamingStorage
+        {
+            get
+            {
+                throw new PlatformNotSupportedException("The Roaming folder is not supported in Android !");
+            }
+        }
+
+        public IFile GetFileFromPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new FormatException("Null or emtpy *path* argument not allowed !");
+            }
+
+            if (System.IO.File.Exists(path))
+            {
+                return new File(path);
+            }
+
+            return null;
+        }
+
+        public IFolder GetFolderFromPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new FormatException("Null or emtpy *path* argument not allowed !");
+            }
+
+            if (Directory.Exists(path))
+            {
+                return new Folder(path);
+            }
+
+            return null;
+        }
+    }
+}
